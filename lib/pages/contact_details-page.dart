907c76543fd6af3dc67df0bbc1a610913b75ee22
zahid2873 <mahdi.zhm13@gmail.com>
2023-03-27 03:45:11 +0600

    url launcher commit

--- lib/pages/contact_details-page.dart
+++ lib/pages/contact_details-page.dart
@@ -1,8 +1,10 @@
 import 'package:contactapp/providers/contact_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 import '../models/contact_model.dart';
+import '../utils/helper_function.dart';
 
 class ContactDetailsPage extends StatefulWidget {
   static const String routeName = '/details';
@@ -46,7 +48,34 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
              final contact = snapshot.data!;
              return ListView(
               children: [
+                Image.asset("images/man-person-icon.png",height: 200,width: 200,),
+                ListTile(
+                  leading: IconButton(onPressed: (){},icon:Icon(Icons.edit)),
+                  title: Text(contact.mobile),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(onPressed: (){
+                        _callContact(contact.mobile);
+                      }, icon: Icon(Icons.call)),
+                      IconButton(onPressed: (){
+                        _smsContact(contact.mobile);
+                      }, icon: Icon(Icons.message)),
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: IconButton(onPressed: (){},icon:Icon(Icons.edit)),
+                  title: Text(contact.email.isEmpty?"Email not set yet": contact.email),
+                  trailing: IconButton(onPressed: (){
+                    if(contact.email.isEmpty){
+                      showMsg(context, "Please first add email");
+                    }else{
+                    _sendEmail(contact.email);
+                    }
+                  },icon: Icon(Icons.email),),
 
+                )
               ],
             );
           }
@@ -59,4 +88,25 @@ class _ContactDetailsPageState extends State<ContactDetailsPage> {
       ),
     );
   }
+
+  _callContact(String mobile) async {
+    final url = 'tel:$mobile';
+    if (await canLaunchUrlString(url)){
+      await launchUrlString(url);
+    }else{
+      showMsg(context,"Could not perform this operation");
+
+    }
+  }
+
+   _smsContact(String mobile) async{
+    final url = 'sms:$mobile';
+    if(await canLaunchUrlString(url)){
+      await launchUrlString(url);
+    }else{
+      showMsg(context, "Could not perform this operation");
+    }
+   }
+
+   _sendEmail(String email) {}
 }
---
+++ lib/utils/helper_function.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void showMsg(BuildContext context, String msg){
+   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(msg)));
+}
